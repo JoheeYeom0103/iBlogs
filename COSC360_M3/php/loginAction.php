351f972efc6d20091fbb64dbbb29e91dd7bd0af9
tatkg24 <tatum@atomic55.net>
2024-03-16 17:50:05 -0700

    Fixed Bug

--- COSC360_M3/php/loginAction.php
+++ COSC360_M3/php/loginAction.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // get input from the form
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $hashedPass = md5($password);
+    $hashedPassword = trim(md5($password));
 
     $errors = isEmptyLogin($username, $password);
 
@@ -19,56 +19,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt =  mysqli_prepare($connection, $sql);
         // Bind parameters to the query
         mysqli_stmt_bind_param($stmt, "s", $username);
+
         // Execute the prepared statement
         mysqli_stmt_execute($stmt);
+
         // Get the result set
         $result = mysqli_stmt_get_result($stmt);
 
         // check to see if we return ONE result from the query
         if(mysqli_num_rows($result) == 1){
             $row = mysqli_fetch_assoc($result);
-            $dbPass = $row["Password"];
-
-            // check to see if the pass matches
-            if($hashedPass === $dbPass) {
+            $dbPass = trim($row["Password"]);
+            if($hashedPassword === $dbPass){
                 // set the session for the user
                 $_SESSION['username'] = $username;
                 // send to home page
                 header("Location: ../explore.php"); 
                 exit();
-            }else {
-                $errors[] = "<p> Invalid Password. </p>";
-                 // Store errors in session
-                $_SESSION['loginErrors'] = $errors;
-                
-                // Redirect back to login page
-                header("Location: ../loginPage.php");
-                exit();
+            }else{
+                $errors[] = "Password is incorrect";
             }
-        }else {
-            $errors[] = "<p> Cannot locate an account with the specified username </p>";
-             // Store errors in session
-             $_SESSION['loginErrors'] = $errors;
-                
-             // Redirect back to login page
-             header("Location: ../loginPage.php");
-             exit();
+        }else{
+            // set the errors array to include a message about the invalid credentials
+            $errors[] = "Username or password is incorrect";
         }
-            
-    }else{
-        // set the errors array to include a message about the invalid credentials
-        $errors[] = "Username or password is incorrect";
-         // Store errors in session
-         $_SESSION['loginErrors'] = $errors;
-                
-         // Redirect back to login page
-         header("Location: ../loginPage.php");
-         exit();
-    }
-    
         
+        // end the prepared statement
+        mysqli_stmt_close($stmt);
     }
 
+    // Store errors in session
+    $_SESSION['loginErrors'] = $errors;
+    
+    // Redirect back to login page
+    header("Location: ../loginPage.php");
+    exit();
+}
 
 // close DB connection after the script finishes running
 mysqli_close($connection);
